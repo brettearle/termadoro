552f53c815bbd6cc1bc6a9c2b7792c820b279a12
Brett <brettearle@gmail.com>
2025-04-27 13:13:07 +0930

    feat<renderer>WIP

--- art/render.go
+++ art/render.go
@@ -1,16 +1,47 @@
+// art is the renderer for tdoro
 package art
 
+import "io"
+
+// Is a single frame of a moving picture
 type Frame []byte
 
+// State of moving picture
 type Reel struct {
-	count  int
-	frames []*Frame
+	currentFrame int
+	frames       []*Frame
 }
 
 func newReel(frs []*Frame) (Reel, error) {
 	return Reel{
-		count:  0,
-		frames: frs,
+		currentFrame: 0,
+		frames:       frs,
 	}, nil
 
+}
+
+// Draws current frame in Reel to writer
+func (r *Reel) Draw(w io.Writer) {
+	w.Write(*r.frames[r.currentFrame])
+}
+
+// Moves currentFrame to next frame in sequence
+// If last frame in sequence goes back to the first
+func (r *Reel) Next() {
+	if r.currentFrame == len(r.frames)-1 {
+		r.currentFrame = 0
+	} else {
+		r.currentFrame += 1
+	}
+
+}
+
+// TODO:
+func NewClockHeadWorkReel() Reel {
+	return Reel{}
+}
+
+// TODO:
+func NewClockHeadRestReel() Reel {
+	return Reel{}
 }
--- art/render_test.go
+++ art/render_test.go
@@ -1,24 +1,63 @@
 package art
 
 import (
+	"bytes"
 	"reflect"
 	"testing"
 )
 
-func TestRender(t *testing.T) {
+func setUp() ([]*Frame, Reel) {
 	var testFrame1 Frame = []byte("-")
 	var testFrame2 Frame = []byte("--")
 	var testFrame3 Frame = []byte("---")
 	var testFrame4 Frame = []byte("--->")
+
+	testFrames := []*Frame{&testFrame1, &testFrame2, &testFrame3, &testFrame4}
+	testReel := Reel{
+		currentFrame: 0,
+		frames:       testFrames,
+	}
+	return testFrames, testReel
+}
+
+func TestRender(t *testing.T) {
 	t.Run("newReel", func(t *testing.T) {
-		testFrames := []*Frame{&testFrame1, &testFrame2, &testFrame3, &testFrame4}
+		testFrames, want := setUp()
 		got, _ := newReel(testFrames)
-		want := Reel{
-			count:  0,
-			frames: testFrames,
-		}
 		if !reflect.DeepEqual(got, want) {
 			t.Errorf("want %+v but got %+v", want, got)
+		}
+	})
+
+	t.Run("reel.Draw", func(t *testing.T) {
+		testFrames, testReel := setUp()
+		got := new(bytes.Buffer)
+		testReel.Draw(got)
+		want := string(*testFrames[0])
+		if got.String() != want {
+			t.Errorf("want %s but got %s", want, got)
+		}
+	})
+
+	t.Run("reel.Next", func(t *testing.T) {
+		_, testReel := setUp()
+		testReel.Next()
+		got := testReel.currentFrame
+		want := 1
+		if got != want {
+			t.Errorf("want %d but got %d", want, got)
+		}
+	})
+
+	t.Run("reel.Next should reset to 1st frame if at last frame of sequence", func(t *testing.T) {
+		_, testReel := setUp()
+		// set current frame to the last frame in sequence
+		testReel.currentFrame = 3
+		testReel.Next()
+		got := testReel.currentFrame
+		want := 0
+		if got != want {
+			t.Errorf("want %d but got %d", want, got)
 		}
 	})
 }
